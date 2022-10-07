2a2edfac656028c9840173817347588b6c3bbcb7
olegphenomenon <oleg.hasjanov@eestiinternet.ee>
2022-10-07 10:12:12 +0300

    added rabbitmq and rpc

--- app/routes/geocoder_routes.rb
+++ app/routes/geocoder_routes.rb
@@ -1,35 +1,35 @@
-class GeocoderRoutes < Application
-  get '/' do
-    geocoder_params = validate_with!(GeocoderParamsContract)
-    result = Geocoder::SearchService.call(*geocoder_params.to_h.values)
+# class GeocoderRoutes < Application
+#   get '/' do
+#     geocoder_params = validate_with!(GeocoderParamsContract)
+#     result = Geocoder::SearchService.call(*geocoder_params.to_h.values)
 
-    if result.present?
-      status 200
-      json serialize_data(result)
-    else
-      status 404
-      json serialize_error
-    end
-  end
+#     if result.present?
+#       status 200
+#       json serialize_data(result)
+#     else
+#       status 404
+#       json serialize_error
+#     end
+#   end
 
-  private
+#   private
 
-  def serialize_data(data)
-    lat = data[0]
-    lon = data[1]
+#   def serialize_data(data)
+#     lat = data[0]
+#     lon = data[1]
 
-    { data: {
-        lat: lat,
-        lon: lon
-      }
-    }
-  end
+#     { data: {
+#         lat: lat,
+#         lon: lon
+#       }
+#     }
+#   end
 
-  def serialize_error
-    {
-      data: {
-        errors: I18n.t(:not_found, scope: 'api.errors')
-      }
-    }
-  end
-end
+#   def serialize_error
+#     {
+#       data: {
+#         errors: I18n.t(:not_found, scope: 'api.errors')
+#       }
+#     }
+#   end
+# end
